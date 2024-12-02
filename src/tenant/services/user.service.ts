073ef123a995857611b4dd775fe0bf256e15ccf1
Acce117 <e.carralero117@gmail.com>
2024-12-02 12:15:47 -0500

    user tests adapted to new relations

--- src/tenant/services/user.service.ts
+++ src/tenant/services/user.service.ts
@@ -3,13 +3,20 @@ import { User } from '../models/user.entity';
 
 export class UserService extends CrudBaseService(User) {
     public async userTests(user_id: number) {
-        // const user: User = await this.getOne(
-        //     {
-        //         relations: ['group.psiTests'],
-        //     },
-        //     user_id,
-        // );
+        const user: User = await this.getOne(
+            {
+                relations: [
+                    {
+                        name: 'groups',
+                        relations: [{ name: 'psiTests' }],
+                    },
+                ],
+            },
+            user_id,
+        );
 
-        // return user?.group ? user.group.psiTests : [];
+        return user?.groups
+            ? user.groups.forEach((group) => group.psiTests)
+            : [];
     }
 }
