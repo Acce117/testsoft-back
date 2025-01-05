eda2a035d95961ec6c526fdb7a367eb74ad0c223
Acce117 <e.carralero117@gmail.com>
2025-01-04 23:45:59 -0500

    refactored file handler

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,10 +1,15 @@
 import { Global, Module } from '@nestjs/common';
 import { QueryFactory } from './services/query-factory';
-import { FileHandlerService } from './services/file-handler.service';
+import { FSFileHandler } from './services/file-handler';
+import { AsyncLocalStorage } from 'async_hooks';
 
 @Module({
-    providers: [QueryFactory, FileHandlerService],
-    exports: [QueryFactory, FileHandlerService],
+    providers: [
+        QueryFactory,
+        FSFileHandler,
+        { provide: AsyncLocalStorage, useValue: new AsyncLocalStorage() },
+    ],
+    exports: [QueryFactory, FSFileHandler, AsyncLocalStorage],
 })
 @Global()
 export class CommonModule {}
--- src/common/services/file-handler.ts
+++ src/common/services/file-handler.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { createWriteStream, existsSync, rmSync } from 'fs';
 
+export interface FileHandler {
+    saveFile(destination, file);
+}
+
 @Injectable()
-export class FileHandlerService {
-    saveFileInFS(destination, file: Express.Multer.File) {
+export class FSFileHandler implements FileHandler {
+    saveFile(destination: any, file: any) {
         const file_path = `${destination}${Date.now()}.${file.mimetype.split('/')[1]}`;
         try {
             const f = createWriteStream(file_path);
@@ -14,15 +18,18 @@ export class FileHandlerService {
         }
     }
 
-    saveFileIn3rd(destination, file) {
-        //TODO implement
-    }
-
     private deleteFile(file_path: string) {
         const result = existsSync(file_path);
         if (result) {
             rmSync(file_path);
         }
         return result;
+    }
+}
+
+@Injectable()
+export class ExternalFileHandler implements FileHandler {
+    saveFile(destination: any, file: any) {
+        throw new Error('Method not implemented.');
     }
 }
