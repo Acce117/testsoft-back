76eef1b13ca35f6cd76085b3cbd3d1744aa0f7d1
Acce117 <e.carralero117@gmail.com>
2025-03-30 23:47:22 -0400

    Update group.service.ts
    parsing to int limit and offset

--- src/tenant/services/group.service.ts
+++ src/tenant/services/group.service.ts
@@ -63,8 +63,8 @@ export class GroupService extends TreeBaseService({ model: Group }) {
         const users = [];
         data.forEach((group) => users.push(...group.users));
 
-        let limit = params.limit || 10;
-        const offset = params.offset || 0;
+        let limit = parseInt(params.limit) || 10;
+        const offset = parseInt(params.offset) || 0;
 
         if (offset + limit > users.length - 1) limit = users.length % limit;
 
