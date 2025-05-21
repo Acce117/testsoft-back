dc62619067b4a6a29cf1e5f44f9c8003cd3be9fa
Acce117 <e.carralero117@gmail.com>
2025-05-21 01:07:05 -0400

    report fixed again

--- src/reports/models/PreferredAvoidedRoles.model.ts
+++ src/reports/models/PreferredAvoidedRoles.model.ts
@@ -4,21 +4,15 @@ import { ViewColumn, ViewEntity } from 'typeorm';
     name: 'preferred_avoided_roles',
 })
 export class PreferredAvoidedRoles {
-    @ViewColumn({ name: 'id_group' })
-    id_group: number;
-
     @ViewColumn({ name: 'fk_id_group' })
     fk_id_group: string;
 
-    @ViewColumn({ name: 'id_rol' })
-    id_rol: number;
+    @ViewColumn({ name: 'role_name' })
+    role_name: number;
 
-    @ViewColumn({ name: 'rol_name' })
-    rol_name: number;
+    @ViewColumn({ name: 'preferred' })
+    preferred: number;
 
-    @ViewColumn({ name: 'how_many_preferred' })
-    how_many_preferred: number;
-
-    @ViewColumn({ name: 'how_many_avoided' })
-    how_many_avoided: number;
+    @ViewColumn({ name: 'avoided' })
+    avoided: number;
 }
