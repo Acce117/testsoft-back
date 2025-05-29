6b78f9abd6675cbe784020134a19c74f5bcb26e7
Acce117 <e.carralero117@gmail.com>
2025-05-29 00:45:12 -0400

    infinite event dispatcher, after update removed for now

--- src/psiTest/models/answer.subscriber.ts
+++ src/psiTest/models/answer.subscriber.ts
@@ -3,7 +3,6 @@ import {
     EntitySubscriberInterface,
     EventSubscriber,
     InsertEvent,
-    UpdateEvent,
 } from 'typeorm';
 import { Answer } from './answer.entity';
 import { FileHandler, FSFileHandler } from 'src/common/services/file-handler';
@@ -24,19 +23,6 @@ export class AnswerSubscriber implements EntitySubscriberInterface {
 
     afterInsert(event: InsertEvent<Answer>): Promise<any> | void {
         const entity: Answer = event.entity;
-
-        if (entity.file) {
-            const path = this.fileHandler.saveFile(entity.file);
-            const image = new Image();
-            image.url = path;
-            image.answer = entity;
-
-            image.save();
-        }
-    }
-
-    afterUpdate(event: UpdateEvent<Answer>): Promise<any> | void {
-        const entity: Answer = event.entity as Answer;
 
         if (entity.file) {
             const path = this.fileHandler.saveFile(entity.file);
