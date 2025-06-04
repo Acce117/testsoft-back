501ad0e53e72e48e26f83679081982fc57517133
Acce117 <e.carralero117@gmail.com>
2025-06-03 23:58:46 -0400

    defining analyst as permitted role

--- src/psiTest/controllers/correctAnswer.controller.ts
+++ src/psiTest/controllers/correctAnswer.controller.ts
@@ -1,7 +1,10 @@
 import { CrudBaseController } from 'src/common/controllers/controller';
 import { CorrectAnswerService } from '../services/correctAnswer.service';
+import { UseGuards } from '@nestjs/common';
+import { RoleGuard, Roles } from 'src/tenant/guards/RoleGuard.guard';
 
 export class CorrectAnswerController extends CrudBaseController({
     prefix: 'correct_answer',
     service: CorrectAnswerService,
+    decorators: [UseGuards(RoleGuard), Roles(['Analyst'])],
 }) {}
--- src/psiTest/controllers/question.controller.ts
+++ src/psiTest/controllers/question.controller.ts
@@ -1,10 +1,13 @@
 import { CrudBaseController } from 'src/common/controllers/controller';
 import { QuestionService } from '../services/question.service';
 import { FileInBody } from 'src/common/decorators/fileInBody.decorator';
+import { UseGuards } from '@nestjs/common';
+import { RoleGuard, Roles } from 'src/tenant/guards/RoleGuard.guard';
 
 export class QuestionController extends CrudBaseController({
     prefix: 'question',
     service: QuestionService,
+    decorators: [UseGuards(RoleGuard), Roles(['Analyst'])],
     create: {
         decorators: [FileInBody('file')],
     },
