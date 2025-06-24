2d40487bccb880b5e9b1036be4e47eb787f0f95b
Acce117 <e.carralero117@gmail.com>
2025-06-24 04:33:26 -0400

    fix error with deleting answers

--- src/psiTest/services/answer.service.ts
+++ src/psiTest/services/answer.service.ts
@@ -1,7 +1,25 @@
 import { CrudBaseService } from 'src/common/services/service';
 import { Answer } from '../models/answer.entity';
+import { TributeService } from './tribute.service';
+import { Inject } from '@nestjs/common';
+import { EntityManager } from 'typeorm';
+import { Tribute } from '../models/tribute.entity';
 
 export class AnswerService extends CrudBaseService({
     model: Answer,
     delete: 'hard',
-}) {}
+}) {
+    @Inject(TributeService) tributeService: TributeService;
+
+    async delete(id: any, manager?: EntityManager) {
+        const tributes = await this.tributeService.getAll({
+            where: {
+                fk_id_answer: id,
+            },
+        });
+
+        await manager.withRepository(Tribute.getRepository()).delete(tributes);
+
+        return super.delete(id, manager);
+    }
+}
